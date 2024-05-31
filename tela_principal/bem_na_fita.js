32180befc6c4c300a7593a70557f334c48c84ffc
Enzo Custodio <enzo.19@hotmail.com>
2024-05-31 16:04:52 -0300

    fvck

---
+++ tela_principal/bem_na_fita.js
@@ -0,0 +1,5 @@
+function Tela_cliente(){
+    window.location.href = "../tela_add_cliente/tela_add_cliente.html";
+    //o gepeto ta achando qeu tu quer fazer uma requisição HTML
+    //console.log('teste')
+}
